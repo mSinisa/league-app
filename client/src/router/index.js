00fd88e0db084316798885a140dd063a8780358e
mSinisa <msinisa602@yahoo.com>
2020-04-09 16:59:38 +0200

    lazy route loading

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,15 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-import Login from '../views/Login'
-import About from '../views/About'
-import Register from '../views/Register'
-import AdminHome from '../views/AdminHome'
-import NewDay from '../views/day/New'
-import NewDivision from '../views/division/New'
-// import EditDivision from '../views/division/Edit'
-import NewTeam from '../views/team/New'
-import ShowTeam from '../views/team/Show'
 
 Vue.use(VueRouter)
 
@@ -21,7 +12,7 @@ const routes = [{
     {
         path: '/about',
         name: 'About',
-        component: About,
+        component: () => import(/* webpackChunkName: "About" */ '../views/About'),
         meta: {
             requiresAuth: true
         }
@@ -29,45 +20,42 @@ const routes = [{
     {
         path: '/login',
         name: 'Login',
-        component: Login
+        component: () => import(/* webpackChunkName: "Login" */ '../views/Login'),
     },
     {
         path: '/register',
         name: 'Register',
-        component: Register
+        component: () => import(/* webpackChunkName: "Register" */ '../views/Register'),
     },
     {
         path: '/admin',
         name: 'AdminHome',
-        component: AdminHome
+        component: () => import(/* webpackChunkName: "AdminHome" */ '../views/AdminHome'),
+        meta: {
+            requiresAuth: true, isAdmin: true
+        }
     },
     {
         path: '/admin/days/new',
         name: 'NewDay',
-        component: NewDay
+        component: () => import(/* webpackChunkName: "NewDay" */ '../views/day/New'),
     },
     {
         path: '/admin/days/:dayId/divisions/new',
         name: 'NewDivision',
-        component: NewDivision,
+        component: () => import(/* webpackChunkName: "NewDivision" */ '../views/division/New'),
         props: true
     },
-    // {
-    //   path: '/admin/days/:dayId/divisions/:divisionId/edit',
-    //   name: 'EditDivision',
-    //   component: EditDivision,
-    //   props: true
-    // },
     {
         path: '/admin/days/:dayId/divisions/:divisionId/teams/new',
         name: 'NewTeam',
-        component: NewTeam,
+        component: () => import(/* webpackChunkName: "NewTeam" */ '../views/team/New'),
         props: true
     },
     {
         path: '/admin/days/:dayId/divisions/:divisionId/teams/:teamId',
         name: 'ShowTeam',
-        component: ShowTeam,
+        component: () => import(/* webpackChunkName: "About" */ '../views/team/Show'),
         props: true
     }
 ]
@@ -79,10 +67,27 @@ const router = new VueRouter({
 })
 
 router.beforeEach((to, from, next) => {
-    const loggedIn = localStorage.getItem('user')
-    if (to.matched.some(record => record.meta.requiresAuth) && !loggedIn) {
-        next('/')
-    } else {
+    const loggedIn = JSON.parse(localStorage.getItem('user'))
+    const requiresAuth = (to.matched.some(record => record.meta.requiresAuth))
+    const requiresAdmin = (to.matched.some(record => record.meta.isAdmin))
+ 
+    if(requiresAuth && !loggedIn){
+        console.log('not logged in')
+        next({name: 'Login'})
+    } else if(requiresAuth && !requiresAdmin && loggedIn) {
+        console.log('doesnt require admin')
+        next()
+    } else if(requiresAuth && requiresAdmin && loggedIn){
+        if(loggedIn.isAdmin) {
+            console.log('aparently is admin')
+            next()
+        } else {
+            console.log('not ad admin cant go')
+            next('/')
+        }
+    }
+    else {
+        console.log('everyone goes')
         next()
     }
 })
