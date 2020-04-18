9d43c1b8106c3ea991a9c7555cf9c238e7bd6b7a
mSinisa <msinisa602@yahoo.com>
2020-04-18 06:07:56 +0200

    updated User model

--- server/models/User.js
+++ server/models/User.js
@@ -1,24 +1,32 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
-// Role = require('../helpers/role')
 
 let userSchema = new Schema({
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    phoneNumber: String,
-    // teams: [],
-    firstName: String,
-    lastName: String,
-    isAdmin: {type: Boolean, default: false },
-    primaryClub: String
+	email: {
+		type: String,
+		required: [true, 'Email is required'],
+		unique: true,
+		match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+	},
+	password: {
+		type: String,
+		required: [true, 'Password is required']
+	},
+	phoneNumber: String,
+	firstName: { 
+		type: String,
+		required: [true, 'First name is required']
+	},
+	lastName: { 
+		type: String,
+		required: [true, 'Last name is required']
+	},
+	isAdmin: {
+		type: Boolean, 
+		default: false 
+	},
+	primaryClub: String
+	// teams: []
 })
 
 module.exports = mongoose.model('User', userSchema)
