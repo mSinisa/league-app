9c3fc553a472966b732fbeb38f50e759a1cc2119
mSinisa <msinisa602@yahoo.com>
2020-04-18 06:10:53 +0200

    added auth route

--- server/app.js
+++ server/app.js
@@ -5,7 +5,8 @@ const cors = require('cors')
 const dotenv = require('dotenv')
 dotenv.config({ path: './config.env' })
 
-const userRoutes = require('./routes/user/user')
+const authRoutes = require('./routes/auth')
+const userRoutes = require('./routes/user')
 const adminRoutes = require('./routes/admin/admin')
 const dayRoutes = require('./routes/admin/days')
 const divisionRoutes = require('./routes/admin/divisions')
@@ -17,35 +18,40 @@ const app = express()
 app.use(bodyParser.json())
 app.use(cors())
 
-mongoose.connect(process.env.MONGO_URI, { 
-    useNewUrlParser: true, 
-    useUnifiedTopology: true 
-})
-.then(console.log("db connected"))
-.catch(err => {
-    console.log(err)
-})
+mongoose
+	.connect(process.env.MONGO_URI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(console.log('db connected'))
+	.catch((err) => {
+		console.log(err)
+	})
 
 //ROUTES
+app.use(authRoutes)
 app.use(userRoutes)
-app.use("/admin", adminRoutes)
-app.use("/admin/days", dayRoutes)
-app.use("/admin/days/:dayId/divisions", divisionRoutes)
+app.use('/admin', adminRoutes)
+app.use('/admin/days', dayRoutes)
+app.use('/admin/days/:dayId/divisions', divisionRoutes)
 app.use('/admin/days/:dayId/divisions/:divisionId/teams', teamRoutes)
-app.use('/admin/days/:dayId/divisions/:divisionId/teams/:teamId/teamPlayers', teamPlayerRoutes)
+app.use(
+	'/admin/days/:dayId/divisions/:divisionId/teams/:teamId/teamPlayers',
+	teamPlayerRoutes
+)
 
-app.use((req, res, next) => {
-    res.status(404).json({error:{message: 'Page not found'}})
+app.use((req, res) => {
+	res.status(404).json({ error: { message: 'Page not found' } })
 })
 
 // handle errors
-app.use((error, req, res, next) => {
-    res.status(error.status || 500).json({ 
-        error: { message: error.message || 'Oops!! Something went wrong.'}
-    })
-})
+// app.use((error, req, res, next) => {
+// 	res.status(error.status || 500).json({
+// 		error: { message: error.message || 'Oops!! Something went wrong.' },
+// 	})
+// })
 //SERVER
 const port = 8082
 app.listen(port, () => {
-    console.log(`app listening on port ${port}`)
-});
+	console.log(`app listening on port ${port}`)
+})
